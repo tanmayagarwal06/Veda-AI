1db6b0cf09a8b9a290bcd7f4698ba89e6fc98c3b
tanmayagarwal06 <tanmayagarwal125@gmail.com>
2026-05-28 17:14:44 +0530

    Fix scroll clipping by adding min-h-0 to main flex item

--- frontend/src/components/layout/MainLayout.tsx
+++ frontend/src/components/layout/MainLayout.tsx
@@ -13,7 +13,7 @@ export function MainLayout({ children }: MainLayoutProps) {
 
       <div className="flex-1 flex flex-col ml-[228px] min-w-0 h-screen overflow-hidden">
         <Header />
-        <main className="flex-1 overflow-y-auto">
+        <main className="flex-1 min-h-0 overflow-y-auto">
           {children}
         </main>
       </div>
